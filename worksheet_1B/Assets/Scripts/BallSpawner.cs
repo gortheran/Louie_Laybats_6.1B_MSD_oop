f4a46149c455489e5c3dd2ab7fb971b2af4d2b53
gortheran <52323951+gortheran@users.noreply.github.com>
2020-10-27 13:27:08 +0100

    original version as i backtracked to be able to correctly include the mouse and physics

---
+++ worksheet_1B/Assets/Scripts/BallSpawner.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BallSpawner : MonoBehaviour
+{
+    public GameObject Ball;
+    public float padding;
+    private float XMin, XMax, YMin, YMax;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Camera mainCamera = Camera.main;
+        XMin = mainCamera.ViewportToWorldPoint(new Vector3(0,0,0)).x + padding;
+        XMax = mainCamera.ViewportToWorldPoint(new Vector3(1,0,0)).x - padding;
+        YMin = mainCamera.ViewportToWorldPoint(new Vector3(0,0,0)).y + padding;
+        YMax = mainCamera.ViewportToWorldPoint(new Vector3(0,1,0)).y - padding;
+
+        Instantiate(Ball, new Vector3(XMin, YMin, 0), Quaternion.identity);
+        Instantiate(Ball, new Vector3(XMin, YMax, 0), Quaternion.identity);
+        Instantiate(Ball, new Vector3(XMax, YMin, 0), Quaternion.identity);
+        Instantiate(Ball, new Vector3(XMax, YMax, 0), Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
